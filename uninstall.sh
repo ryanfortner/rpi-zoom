0514cafe256e8cfba12a986073717d2e41672afe
Ryan Fortner <71036629+chunky-milk@users.noreply.github.com>
2021-09-01 16:18:24 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -16,7 +16,7 @@ rm -r $HOME/zoom
 rm -f $HOME/zoom.tar.xz
 rm -f $HOME/zoom_x64_libs
 rm -f $HOME/zoom_x64_libs.zip
-rm $HOME/.local/share/applications/zoom.desktop && rm $HOME/.local/share/applications/zupdate.desktop && rm $HOME/.config/autostart/zupdate.desktop || error "Failed to remove desktop shortcuts"
+rm $HOME/.local/share/applications/zoom.desktop && rm -f $HOME/.local/share/applications/zupdate.desktop && rm -f $HOME/.config/autostart/zupdate.desktop || error "Failed to remove desktop shortcuts"
 rm $HOME/Desktop/zoom.desktop
 
 echo "Have a nice day."
